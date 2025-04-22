74ba6b54ad88a6d28b99c2c0efdfa397fac09c8d
Nikitamishra2511 <nmishra1@chicagobooth.edu>
2025-04-22 00:42:26 +0000

    2104

--- app.rb
+++ app.rb
@@ -1,9 +1,25 @@
 require "sinatra"
+configure :development do
 require "sinatra/reloader"
 
+
+end
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+ "Hello World"
+end
+get("/zebra") do
+  "we must add a route to each path we want to support"
+end
+get("/giraffe")do
+"Hopefully this shows up without having to restart the server
+2d6 = https://special-palm-tree-wrx969gjjg57h5qj-3000.app.github.dev/dice/2/6
+"end
+get ("/dice/2/6")do
+first_die = rand(1..6)
+second_die = rand(1..6)
+sum= first_die + second_die
+outcome ="<h1>2d6</h1>
+  <p> You rolled a #{first_die} and a #{second_die} for a total of #{sum}</p>
+"
+outcome
 end
